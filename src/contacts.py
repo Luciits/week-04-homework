f706f61dfae6285ac170db30cd0a313c7a4a3838
Arturs Luters <artursluters@gmail.com>
2026-03-03 16:39:40 +0100

    Pabeigta kontaktu grāmatiņa ar CLI apstrādi

--- src/contacts.py
+++ src/contacts.py
@@ -13,6 +13,7 @@ def load_contacts():
         return [] # Atgriež tukšu sarakstu, ja fails vēl nav izveidots
     with open(CONTACTS_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
+ 
 def save_contacts(contacts):
     """
     Saglabā kontaktu sarakstu JSON failā.
@@ -20,17 +21,21 @@ def save_contacts(contacts):
     with open(CONTACTS_FILE, "w", encoding="utf-8") as f:
         # indent=2 padara JSON failu cilvēkiem lasāmu
         json.dump(contacts, f, indent=2, ensure_ascii=False)
-def add_contacts(name, phone):
+
+def add_contacts(kontaktu_saraksts):
     """
-    Ielādē esošos datus no faila, pievieno jaunu kontaktu kā vārdnīcu un pievieno to sarakstam.
-    saglabā visu sarakstu JSON failā.
+    Pievieno jaunu kontaktu kontaktu sarakstam
     """
-    contacts = load_contacts()
-    new_contact = {"name": name, "phone": phone}
-    contacts.append(new_contact)
-    save_contacts(contacts)
+    name = input("Ievadiet kontakta vārdu: ")
+    phone = input("Ievadiet tālruņa nr.: ")
+    kontaktu_saraksts.append({"name": name, "phone": phone})
     print(f"✓ Pievienots: {name} ({phone})")
+    return True
+
 def list_contacts():
+    """
+    Parāda esošo kontaktu sarakstu
+    """
     contacts = load_contacts()
     if not contacts:
         print("Saraksts ir tukšs.")
@@ -39,10 +44,21 @@ def list_contacts():
     # i ir skaitītājs, c is pati kontakta vārdnīca
     for i, c in enumerate(contacts, 1):
         print(f"{i}. {c['name']} - {c['phone']}")
+
 def search_contacts(query):
+    """
+    Izveido jaunu sarkatu ar meklētajiem kontaktiem
+    """
     contacts = load_contacts()
     # Izveidojam jaunu sarakstu tikai ar to kas atbilst meklētajam
     found = [c for c in contacts if query.lower() in c['name'].lower()]
     print(f"Atrasti {len(found)} kontakti:")
     for i, c in enumerate(found, 1):
         print(f"{i}. {c['name']} - {c['phone']}")
+
+if __name__ == "__main__":
+    kontaktu_saraksts = load_contacts()
+    add_contacts(kontaktu_saraksts)
+    save_contacts(kontaktu_saraksts)
+    list_contacts()
+    search_contacts("Artūrs")
